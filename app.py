d9d8376c6ff001743acfe4e42c7c31df9a98a601
Hisaki Nishimura <hisaki0407@gmail.com>
2023-09-27 15:19:12 +0900

    ファイル名に撮影日時をつけて一位になるよう修正

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, render_template, request, redirect, url_for
 
 #Flaskクラスをインスタンス化して変数appに割り当てる
@@ -17,8 +18,18 @@ def upload():
     if 'image' in request.files:
         image = request.files['image']
         if image.filename != '':
+            # ファイルの拡張子を取得
+            root, ext = os.path.splitext(image.filename)
+
+            # ファイル名に使う日時を取得
+            t_delta = datetime.timedelta(hours=9)
+            JST = datetime.timezone(t_delta, 'JST')
+            now = datetime.datetime.now(JST)
+            image.filename = '{:%Y%m%d%H%M%S}'.format(now) + ext  # 文字列のformatメソッド
+
             # ファイルの保存先パスを構築
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+
             # 画像を保存
             image.save(file_path)
             return '画像がアップロードされました: ' + image.filename
